25f7af4d37a7c8f721da71f0a8429441187703e3
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-28 19:08:17 -0700

    Remove bodyPArser. Use express.json()

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -8,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/", function(req, res) {
     res.end("e-track");
