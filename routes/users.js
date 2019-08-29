88879ee50df2e24fa50373aa11460621d32cb344
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-28 21:41:37 -0700

    Have get request to '/' return all users from User collection. Have post request to '/add' add new user to db and return success message.

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,52 @@ const express = require("express");
 const router = express.Router;
 const User = require("../models/user.model");
 
-router.route("/").get(function(req, res) {});
+function generate_response(success, message, data) {
+    return {
+        success,
+        message,
+        data
+    };
+}
 
-router.route("/add").post(function(req, res) {});
+// create get request to fetch all users
+router.route("/").get(function(req, res) {
+    User.find()
+        .then(function(all_users) {
+            return res.json(
+                generate_response(
+                    true,
+                    "successfull fetched all users",
+                    all_users
+                )
+            );
+        })
+        .catch(function(err) {
+            return res
+                .status(400)
+                .json(
+                    generate_response(false, "failed to fetch all users", err)
+                );
+        });
+});
+
+// create post request to add all users
+router.route("/add").post(function(req, res) {
+    const username = req.body.username;
+
+    const new_user = new User({ username });
+    new_user
+        .save()
+        .then(function() {
+            return res.json(
+                generate_response(true, `successfully saved ${username}`)
+            );
+        })
+        .catch(function(err) {
+            return res
+                .status(400)
+                .json(generate_response(false, `failed to saved ${username}`));
+        });
+});
 
 module.exports = router;
