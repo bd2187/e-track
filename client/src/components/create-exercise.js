5481b6241c241f30344a89db9512b5b4243e23d5
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-31 19:31:33 -0700

    Add state to CreateExercise component. Create updateTextField() to update state

--- client/src/components/create-exercise.js
+++ client/src/components/create-exercise.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateExercise = function() {
-    return <p>CreateExercise</p>;
+    const [username, setUsername] = useState("");
+    const [description, setDescription] = useState("");
+    const [duration, setDuration] = useState(0);
+    const [date, setDate] = useState(new Date());
+
+    const updateTextField = function(evt) {
+        const field = evt.target.getAttribute("data-type");
+        const value = evt.target.value;
+
+        switch (field) {
+            case "username":
+                setUsername(value);
+                break;
+
+            case "description":
+                setDescription(value);
+                break;
+
+            case "duration":
+                setDuration(value);
+                break;
+            default:
+        }
+    };
+
+    return (
+        <form>
+            <input
+                data-type="username"
+                type="text"
+                onChange={updateTextField}
+                value={username}
+            />
+        </form>
+    );
 };
 
 export default CreateExercise;
