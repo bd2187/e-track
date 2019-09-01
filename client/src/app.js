280139aabc43b34da08087ca9cd1d1d0a16d7804
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-31 18:29:29 -0700

    Initial commit. Create App component

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = function() {
+    return (
+        <div>
+            <h1>e-track</h1>
+        </div>
+    );
+};
+
+render(<App />, document.getElementById("root"));
