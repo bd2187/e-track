189259ede5e047be2ba4a046a6660c683b58dfb8
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-31 18:59:58 -0700

    Import components, Router, Route, and bootstrap. Render Router and Route components

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import ExerciseList from "./components/exercise-list";
+import EditExercise from "./components/edit-exercise";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
 
 const App = function() {
     return (
-        <div>
-            <h1>e-track</h1>
-        </div>
+        <Router>
+            <Navbar />
+            <Route exact path="/" component={ExerciseList} />
+            <Route path="/edit/:id" component={EditExercise} />
+            <Route path="/create" component={CreateExercise} />
+            <Route path="/user" component={CreateUser} />
+        </Router>
     );
 };
 
