06eac71000d8858238a42e341dd3b17a2d1c2880
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-03 20:55:13 -0700

    Import useEffect fn. Use the fn to call setUsers() and setUsername(). Create onSubmit() which will be called for a form submission. Render markup for username and duration fields

--- client/src/components/create-exercise.js
+++ client/src/components/create-exercise.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const CreateExercise = function() {
     const [username, setUsername] = useState("");
     const [description, setDescription] = useState("");
     const [duration, setDuration] = useState(0);
     const [date, setDate] = useState(new Date());
+    const [users, setUsers] = useState([]);
 
-    const updateTextField = function(evt) {
+    useEffect(function() {
+        setUsers(["user1", "me", "user3"]);
+        setUsername("me");
+    }, []);
+
+    const updateFormField = function(evt) {
         const field = evt.target.getAttribute("data-type");
         const value = evt.target.value;
 
@@ -22,19 +28,58 @@ const CreateExercise = function() {
             case "duration":
                 setDuration(value);
                 break;
+
             default:
         }
     };
 
+    const onSubmit = function(evt) {
+        evt.preventDefault();
+
+        const exercise = {
+            username,
+            description,
+            duration,
+            date
+        };
+
+        window.location = "/";
+    };
+
     return (
-        <form>
-            <input
-                data-type="username"
-                type="text"
-                onChange={updateTextField}
-                value={username}
-            />
-        </form>
+        <div>
+            <h3>create new e log</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>username: </label>
+                    <select
+                        className="form-control"
+                        value={username}
+                        onChange={updateFormField}
+                        data-type="username"
+                    >
+                        {users.map(function(user) {
+                            return (
+                                <option key={user} value={user}>
+                                    {user}
+                                </option>
+                            );
+                        })}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>duration (in minutes):</label>
+                    <input
+                        className="form-control"
+                        type="number"
+                        value={duration}
+                        onChange={updateFormField}
+                        data-type="duration"
+                        min="0"
+                    />
+                </div>
+            </form>
+        </div>
     );
 };
 
