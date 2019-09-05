2e5b2201010c187b649506428047bbd9849853e1
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-04 22:18:04 -0700

    Add state and useEffect to exerciselist component. Also ceate delete_exercise fn

--- client/src/components/exercise-list.js
+++ client/src/components/exercise-list.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ExerciseList = function() {
+    const [exercises, setExercises] = useState([]);
+    const delete_exercise = function(id) {
+        axios.delete(`http://localhost:5000/exercises/${id}`).then(res => {
+            const filtered_exercises = exercises.filter(
+                item => item._id !== id
+            );
+
+            setExercises(filtered_exercises);
+        });
+    };
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:5000/exercises/")
+            .then(res => {
+                var exercises_arr = res.data.data;
+                setExercises(exercises_arr);
+            })
+            .catch(err => {});
+    }, []);
     return <p>ExerciseList</p>;
 };
 
