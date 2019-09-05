9164ba8ffc9c7e008134b96fd7db68c76760cc8b
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-04 20:38:30 -0700

    Add markup for createuser

--- client/src/components/create-user.js
+++ client/src/components/create-user.js
@@ -4,15 +4,38 @@ const CreateUser = function() {
     const [username, setUsername] = useState("");
 
     const onUsernameChange = function(evt) {
-        evt.preventDefault();
         const value = evt.target.value;
         setUsername(value);
     };
 
-    const onUsernameSubmit = function() {
+    const onUsernameSubmit = function(evt) {
+        evt.preventDefault();
         setUsername("");
     };
-    return <p>CreateUser</p>;
+
+    return (
+        <div>
+            <h3>Create New User</h3>
+            <form onSubmit={onUsernameSubmit}>
+                <div className="form-group">
+                    <label>username:</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={username}
+                        onChange={onUsernameChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="create user"
+                        className="btn btn-primary"
+                    />
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default CreateUser;
