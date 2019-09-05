301298ac694d47e3fbe75cea03d1f284770f7afc
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-04 20:32:58 -0700

    Add state and onUsernameChange() & onUsernameSubmit functions

--- client/src/components/create-user.js
+++ client/src/components/create-user.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateUser = function() {
+    const [username, setUsername] = useState("");
+
+    const onUsernameChange = function(evt) {
+        evt.preventDefault();
+        const value = evt.target.value;
+        setUsername(value);
+    };
+
+    const onUsernameSubmit = function() {
+        setUsername("");
+    };
     return <p>CreateUser</p>;
 };
 
