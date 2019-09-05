7a433d543cefd1a0ec54bcdec5c3712b96139131
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-04 22:35:32 -0700

    Create Exercise component. Render markup for exerciselist cmponent

--- client/src/components/exercise-list.js
+++ client/src/components/exercise-list.js
@@ -1,5 +1,24 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+
+const Exercise = function(props) {
+    const { username, description, duration, date } = props.exercise;
+    return (
+        <tr>
+            <td>{username}</td>
+            <td>{description}</td>
+            <td>{duration}</td>
+            <td>{date.substring(0, 10)}</td>
+            <td>
+                {/* <Link to={`/edit/${props.exercise._id}`}>edit</Link> |{" "}
+                <a href="#" onClick={props.delete_exercise(props.exercise._id)}>
+                    delete
+                </a> */}
+            </td>
+        </tr>
+    );
+};
 
 const ExerciseList = function() {
     const [exercises, setExercises] = useState([]);
@@ -22,7 +41,35 @@ const ExerciseList = function() {
             })
             .catch(err => {});
     }, []);
-    return <p>ExerciseList</p>;
+
+    const render_exercise_list = function() {
+        return exercises.map(function(exercise) {
+            return (
+                <Exercise
+                    exercise={exercise}
+                    delete_exercise={delete_exercise}
+                    key={exercise._id}
+                />
+            );
+        });
+    };
+    return (
+        <div>
+            <h3>logged exercises</h3>
+            <table className="table">
+                <thead className="thead-light">
+                    <tr>
+                        <th>username</th>
+                        <th>description</th>
+                        <th>duration</th>
+                        <th>date</th>
+                        <th>actions</th>
+                    </tr>
+                </thead>
+                <tbody>{render_exercise_list()}</tbody>
+            </table>
+        </div>
+    );
 };
 
 export default ExerciseList;
